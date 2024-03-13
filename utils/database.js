3ab3a3411b8eef716d98452da0c08ad36fe6ae2a
Nazmus Sakib <sakib.tnt@gmail.com>
2024-03-13 14:14:09 +0600

    static db connecton

--- utils/database.js
+++ utils/database.js
@@ -11,7 +11,7 @@ export const connectToDB = async () => {
     }
 
     try{
-        await mongoose.connect(process.env.MONGODB_URI,{
+        await mongoose.connect("mongodb+srv://sakib57:sakib984@xtreem.wnemiwq.mongodb.net/?retryWrites=true&w=majority&appName=Xtreem",{
             dbName: "share_prompt",
             useNewUrlParser: true,
             useUnifiedTopology: true,
