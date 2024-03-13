3552b1c767844dec71fb500638dd3e79ba585619
Nazmus Sakib <sakib.tnt@gmail.com>
2024-03-13 14:29:50 +0600

    static db connecton

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,68 +1,67 @@
-"use client"
-import Form from "@/components/Form"
+"use client";
+import Form from "@/components/Form";
 import { useRouter, useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react"
+import { Suspense, useEffect, useState } from "react";
 
 const EditPrompt = () => {
   const router = useRouter();
   const searchParams = useSearchParams();
-const promptId = searchParams.get("id");
+  const promptId = searchParams.get("id");
 
   const [submitting, setSubmitting] = useState(false);
   const [post, setPost] = useState({
     prompt: "",
-    tag: ""
+    tag: "",
   });
 
   useEffect(() => {
     const getPromptDetails = async () => {
-        const response = await fetch(`/api/prompt/${promptId}`);
-        const data = await response.json();
-        setPost({
-            prompt: data.prompt,
-            tag: data.tag
-        })
-    }
-    if(promptId) getPromptDetails();
-  
-  }, [promptId])
-  
+      const response = await fetch(`/api/prompt/${promptId}`);
+      const data = await response.json();
+      setPost({
+        prompt: data.prompt,
+        tag: data.tag,
+      });
+    };
+    if (promptId) getPromptDetails();
+  }, [promptId]);
 
-  const updatePrompt = async(e) => {
+  const updatePrompt = async (e) => {
     e.preventDefault();
     setSubmitting(true);
 
-    if(!promptId) return alert ("Prompt ID not found");
+    if (!promptId) return alert("Prompt ID not found");
 
     try {
       const response = await fetch(`/api/prompt/${promptId}`, {
         method: "PATCH",
         body: JSON.stringify({
           prompt: post.prompt,
-          tag: post.tag
-        })
-      })
+          tag: post.tag,
+        }),
+      });
 
-      if(response.ok){
+      if (response.ok) {
         router.push("/");
       }
     } catch (error) {
       console.log(error);
-    }finally{
+    } finally {
       setSubmitting(false);
     }
-  }
-
+  };
 
   return (
-    <Form
-      type="Update"
-      post={post}
-      setPost={setPost}
-      submitting={submitting}
-      handleSubmit={updatePrompt}
-    />
-  )
-}
+    <Suspense>
+      <Form
+        type="Update"
+        post={post}
+        setPost={setPost}
+        submitting={submitting}
+        handleSubmit={updatePrompt}
+      />
+    </Suspense>
+  );
+};
 
-export default EditPrompt
+export default EditPrompt;
